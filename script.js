e8c0f5d1d4a9c458eba6537ce586d5a17b4ce7d8
JumpIsac321 <145168700+JumpIsac321@users.noreply.github.com>
2026-04-14 09:48:30 -0700

    Added remaining javascript features

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // add javascript here
 darkmode = false;
 
-function myFunction() {
+function darkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
   darkmode = !darkmode;
@@ -9,5 +9,30 @@ function myFunction() {
     document.getElementById("darkicon").src = "/image/sunicon.png"
   } else {
     document.getElementById("darkicon").src = "/image/moonicon.png"
+  }
+}
+
+function toggleAnswer() {
+  const answerDiv = document.getElementById("answer");
+  console.log(answerDiv.style.display);
+  if (answerDiv.style.display === "none"){
+    answerDiv.style.display = "inline";
+  } else {
+    answerDiv.style.display = "none";
+  }
+}
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.fontSize = "10px";
+    document.getElementById("darkicon").style.width = "29px";
+    document.getElementById("darkicon").style.height = "29px";
+  } else {
+    document.getElementById("navbar").style.fontSize = "16px";
+    document.getElementById("darkicon").style.width = "35px";
+    document.getElementById("darkicon").style.height = "35px";
   }
 }
