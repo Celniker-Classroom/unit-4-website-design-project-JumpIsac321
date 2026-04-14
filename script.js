725567e794799d467a26a48846c8d1f6c915ae87
JumpIsac321 <145168700+JumpIsac321@users.noreply.github.com>
2026-04-14 08:25:52 -0700

    Started adding darkmode

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // add javascript here
+darkmode = false;
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  darkmode = !darkmode;
+  if (darkmode){
+    document.getElementById("darkicon").src = "/image/sunicon.png"
+  } else {
+    document.getElementById("darkicon").src = "/image/moonicon.png"
+  }
+}
